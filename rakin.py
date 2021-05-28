a0748238a52d2f866e6f6a49d107a6c1278e1071
rakin406 <rakinhacker@gmail.com>
2021-05-28 19:14:31 +0600

    Fix typeerror bug

--- rakin.py
+++ rakin.py
@@ -17,6 +17,7 @@ if ARGS == 0:
 
 bot = Instabot()
 bot.find_person(sys.argv[1])
+print("Opened chat.")
 bot.text_person("Hi, I am Rakin's chatbot. I talk like a human but dumb at most times.")
 
 chatbot = ChatBot("Rakin")
@@ -28,12 +29,11 @@ prev_bot_msg = ""
 
 while True:
     message = bot.get_message()
-    bot_message = chatbot.get_response(message)
+    bot_message = str(chatbot.get_response(message))
 
     # Message must not be the same as last message and it should not be from
     # the chatbot.
     if message != prev_msg and message != prev_bot_msg:
         bot.text_person(bot_message)
-
-    prev_msg = message
-    prev_bot_msg = bot_message
+        prev_msg = message
+        prev_bot_msg = bot_message
