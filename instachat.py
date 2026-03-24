09ff92cd206761f7efbafb44d976e1f8eb7f345e
Rakin Rahman <rakinhacker@gmail.com>
2026-03-25 00:05:18 +0600

    feat: initialize chromedriver

--- instachat.py
+++ instachat.py
@@ -1,7 +1,9 @@
 import os
+import platform
 from time import sleep
+
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 
@@ -11,39 +13,25 @@ class InstaChat:
     """
 
     def __init__(self):
-        # Run headless profiled browser
-        self.__GECKODRIVER = os.path.abspath(os.getcwd()) + "/geckodriver"
-        self.__options = Options()
-        self.__options.headless = False
-        self.__driver = webdriver.Firefox(
-            webdriver.FirefoxProfile(self.__get_profile_path()), \
-                    executable_path=self.__GECKODRIVER, options=self.__options
-        )
+        user_data_dir = self.__get_user_data_dir()
 
-
-    def __rchop(self, s: str, suffix: str) -> str:
-        """
-        Remove last occurence of the substring.
-        """
-        if suffix and s.endswith(suffix):
-            return s[: -len(suffix)]
-        return s
-
-    def __get_profile_path(self) -> str:
-        """
-        Get current Firefox profile path.
-        """
-        # Run headless browser
+        # Set Chromium options
         options = Options()
-        options.headless = True
-        unprofiled_driver = webdriver.Firefox(executable_path=self.__GECKODRIVER, \
-                options=options)
+        options.add_argument(f"--user-data-dir={user_data_dir}")
+        options.add_argument(f"--profile-directory=Default")
+        # options.add_argument("--headless=new")  # Run headless
+        options.add_argument(
+            "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+            "(KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36"
+        )  # Set user agent
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--enable-automation")
+        options.add_argument(
+            "--disable-blink-features=AutomationControlled"
+        )  # Reduces detection
 
-        unprofiled_driver.get("about:profiles")
-        profile = unprofiled_driver.find_elements_by_tag_name("td")[1].text
-        profile = self.__rchop(profile, "Open Directory")
-        unprofiled_driver.quit()
-        return profile
+        self.driver = webdriver.Chrome(options=options)
 
     def open_chat(self, username: str):
         """
@@ -51,7 +39,9 @@ class InstaChat:
         """
         self.__driver.get("https://www.instagram.com/{}/".format(username))
         sleep(2)
-        chat = self.__driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/header/section/div[1]/div[1]/div/div[1]/button/div")
+        chat = self.__driver.find_element_by_xpath(
+            "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div[1]/div[1]/section/main/div/header/section/div[1]/div[1]/div/div[1]/button/div"
+        )
         chat.click()
         sleep(4)
 
@@ -79,3 +69,17 @@ class InstaChat:
         Terminate InstaChat.
         """
         self.__driver.quit()
+
+    def __get_user_data_dir(self) -> str | None:
+        os_name = platform.system()
+        user_data_dir = None
+
+        # Find user data path
+        if os_name == "Windows":
+            user_data_dir = os.path.expandvars(r"%LOCALAPPDATA%\Chromium\User Data")
+        elif os_name == "Linux":
+            user_data_dir = os.path.expanduser("~/.config/chromium")
+        elif os_name == "Darwin":
+            user_data_dir = os.path.expanduser("~/Library/Application Support/Chromium")
+
+        return user_data_dir
